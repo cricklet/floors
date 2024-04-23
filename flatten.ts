0ccb2d576464a23052703e79e9e19834eb1db777
cricklet <kenrick.rilee@gmail.com>
2024-04-23 15:09:15 -0700

    interactions refactor, getting ready for intersect

--- flatten.ts
+++ flatten.ts
@@ -80,3 +80,49 @@ export function flattenScene(source: Readonly<Scene>): Scene {
 
   return flattened;
 }
+
+export function findIntersections(
+  scene: Readonly<Scene>
+): Map<paper.Point, Array<EdgeId>> {
+  const result = new Map<paper.Point, Array<EdgeId>>();
+
+  const edgeIds = Array.from(scene.edges().keys()).toSorted();
+  for (const edgeId of edgeIds) {
+    for (const otherId of edgeIds) {
+      if (edgeId === otherId) {
+        continue;
+      }
+
+      const [point1, point2] = scene.edges().get(edgeId)!;
+      const path = new paper.Path.Line(
+        scene.getPoint(point1),
+        scene.getPoint(point2)
+      );
+
+      const [otherPoint1, otherPoint2] = scene.edges().get(otherId)!;
+      const otherPath = new paper.Path.Line(
+        scene.getPoint(otherPoint1),
+        scene.getPoint(otherPoint2)
+      );
+
+      if (path.intersects(otherPath)) {
+        const intersection = path.getIntersections(otherPath)[0].point;
+        if (
+          intersection.equals(scene.getPoint(point1)) ||
+          intersection.equals(scene.getPoint(point2))
+        ) {
+          continue;
+        }
+
+        if (!result.has(intersection)) {
+          result.set(intersection, []);
+        }
+
+        result.get(intersection)!.push(edgeId);
+        result.get(intersection)!.push(otherId);
+      }
+    }
+  }
+
+  return result;
+}
---
+++ interactions.ts
@@ -0,0 +1,127 @@
+import paper from "paper";
+import { PointId, Scene } from "./scene";
+
+interface Dragging {
+  kind: "dragging";
+  mouseStart: paper.Point;
+  pointStart: paper.Point;
+  selected: PointId;
+}
+
+interface Idle {
+  kind: "idle";
+  selected: PointId | undefined;
+  hover: paper.Point | undefined;
+}
+
+type State = Dragging | Idle;
+
+export type PointRenderState = "hovered" | "selected" | "selected-hovered";
+
+function findPoint(
+  scene: Scene,
+  point: paper.Point
+): {
+  pointId: PointId | undefined;
+  point: paper.Point | undefined;
+} {
+  for (const [pointId, scenePoint] of scene.points()) {
+    if (scenePoint.getDistance(point) < 6) {
+      return {
+        pointId,
+        point: scenePoint,
+      };
+    }
+  }
+
+  return {
+    pointId: undefined,
+    point: undefined,
+  };
+}
+
+export class EditBehavior {
+  paperScope: paper.PaperScope;
+  scene: Scene;
+
+  state: State;
+
+  constructor(paperScope: paper.PaperScope, scene: Scene) {
+    this.paperScope = paperScope;
+    this.scene = scene;
+
+    this.state = { kind: "idle", selected: undefined, hover: undefined };
+
+    this.paperScope.view.onMouseMove = this.onMouseMove.bind(this);
+    this.paperScope.view.onMouseDown = this.onMouseDown.bind(this);
+    this.paperScope.view.onMouseDrag = this.onMouseDrag.bind(this);
+    this.paperScope.view.onMouseUp = this.onMouseUp.bind(this);
+  }
+
+  onMouseMove(event: paper.MouseEvent) {
+    this.state = {
+      kind: "idle",
+      selected: this.state.selected,
+      hover: undefined,
+    };
+
+    const { point } = findPoint(this.scene, event.point);
+    if (point) {
+      this.state.hover = point;
+    }
+  }
+
+  onMouseDown(event: paper.MouseEvent) {
+    this.state = {
+      kind: "idle",
+      selected: undefined,
+      hover: undefined,
+    };
+
+    const { pointId, point } = findPoint(this.scene, event.point);
+    if (pointId && point) {
+      this.state = {
+        kind: "dragging",
+        mouseStart: event.point,
+        pointStart: point,
+        selected: pointId,
+      };
+    }
+  }
+
+  onMouseDrag(event: paper.MouseEvent) {
+    if (this.state.kind !== "dragging") {
+      return;
+    }
+
+    const offset = event.point.subtract(this.state.mouseStart);
+    const newPoint = this.state.pointStart.add(offset);
+    this.scene.setPoint(this.state.selected, newPoint);
+  }
+
+  onMouseUp(event: paper.MouseEvent) {
+    if (this.state.kind === "dragging") {
+      this.state = {
+        kind: "idle",
+        selected: this.state.selected,
+        hover: undefined,
+      };
+    }
+  }
+
+  renderHints(): Array<[paper.Point, PointRenderState]> {
+    const result: Array<[paper.Point, PointRenderState]> = [];
+    if (this.state.kind === "dragging") {
+      result.push([this.scene.getPoint(this.state.selected), "selected"]);
+    } else if (this.state.kind === "idle") {
+      if (this.state.hover) {
+        result.push([this.state.hover, "hovered"]);
+      }
+      if (this.state.selected) {
+        result.push([this.scene.getPoint(this.state.selected), "selected"]);
+      }
+    }
+
+    return result;
+  }
+}
